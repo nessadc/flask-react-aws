745261c88e3c352d11b6bae301f70d92ddde4ed7
Nessad Choudhury <nessadchoudhury@gmail.com>
2021-02-23 13:54:59 -0500

    replacing testdriven with movieapp

--- services/client/src/App.jsx
+++ services/client/src/App.jsx
@@ -31,7 +31,7 @@ class App extends Component {
 
     this.state = {
       users: [],
-      title: "TestDriven.io",
+      title: "MovieApp.io",
       accessToken: null,
       messageType: null,
       messageText: null,
